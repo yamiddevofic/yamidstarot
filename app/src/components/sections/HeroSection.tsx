303f1525d34f9c526ac79a0a3cc79dbe8a3f7ad5
yamiddevofic <yhrodriguez1@hotmail.com>
2025-07-24 18:47:07 -0500

    ajustes hero

--- app/src/components/sections/HeroSection.tsx
+++ app/src/components/sections/HeroSection.tsx
@@ -2,7 +2,7 @@ import { motion } from 'framer-motion';
 import { heroAnimation } from '../../animations/variants';
 
 export const HeroSection = () => (
-  <section id="inicio" className="relative min-h-[65vh] md:min-h-[75vh] flex items-center justify-center pt-20">
+  <section id="inicio" className="relative min-h-[65vh] md:min-h-[75vh] flex items-center justify-center pt-32 md:pt-25">
     <div className="absolute inset-0 z-0">
       <img src="./fondo-animados-estrellas.gif" alt="Yamid Tarot" className="w-full h-full object-cover" />
       <div className="absolute inset-0 bg-gradient-to-b from-gray-900/70 via-gray-900/50 to-gray-900/90" />
@@ -11,7 +11,7 @@ export const HeroSection = () => (
     <div className="relative z-10 container mx-auto px-4 py-12 sm:py-16">
       <motion.div className="text-center max-w-4xl mx-auto" {...heroAnimation}>
         <motion.h1
-          className="text-[3.5rem] sm:text-5xl md:text-6xl lg:text-8xl font-bold mb-6 bg-gradient-to-r from-purple-500 via-pink-400 to-indigo-500 bg-clip-text text-transparent leading-none md:leading-relaxed"
+          className="text-[3rem] sm:text-5xl md:text-6xl lg:text-8xl font-bold mb-6 bg-gradient-to-r from-purple-500 via-pink-400 to-indigo-500 bg-clip-text text-transparent leading-none md:leading-relaxed"
           initial={{ opacity: 0, y: -50 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1, delay: 0.5 }}
@@ -28,15 +28,15 @@ export const HeroSection = () => (
           Transforma tu vida con una lectura de tarot personalizada. Conecta con tu yo interior y desbloquea las respuestas que el universo tiene para ti.
         </motion.p>
 
-        <div className="grid grid-cols-1 gap-4 sm:gap-6 max-w-2xl mx-auto">
+        <div className="grid grid-cols-1 gap-4 sm:gap-6 w-auto max-w-auto place-items-center">
           <a
             href="https://wa.me/+573124673850?text=Hola%2C%20estoy%20interesado%20en%20reservar%20una%20lectura%20de%20tarot%20contigo."
             target="_blank"
             rel="noopener noreferrer"
-            className="w-full flex items-center justify-center"
+            className="w-[80%] md:w-[50%] flex items-center justify-center"
           >
             <motion.button
-              className="w-[50%] px-6 py-3 bg-gradient-to-r from-purple-600 to-pink-600 text-white font-bold rounded-xl shadow-lg hover:shadow-purple-600/40 flex items-center justify-center gap-2 transition-all"
+              className="w-[100%] px-6 py-3 bg-gradient-to-r from-purple-600 to-pink-600 text-white font-bold rounded-xl shadow-lg hover:shadow-purple-600/40 flex items-center justify-center gap-2 transition-all"
               whileHover={{ scale: 1.02, y: -2 }}
               whileTap={{ scale: 0.98 }}
             >
